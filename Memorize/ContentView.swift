08c8d250acd63740a20f2a3d346b5af80d55d357
rurajput <rurajput@expediagroup.com>
2023-03-06 13:51:10 +0530

    Intial Commit with little UI

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,19 +8,72 @@
 import SwiftUI
 
 struct ContentView: View {
+    let emojis = ["😂","😝","😁","😱","👉","🙌","🍻","🔥","🌈","☀","🎈","🌹","💄","🎀","⚽","🎾","🏁","😡","👿","🐻","🐶","🐬","🐟","🍀","👀","🚗","🍎","💝","💙","👌","❤","😍","😉","😓","😳","💪","💩","🍸","🔑","💖","🌟","🎉","🌺","🎶","👠","🏈","⚾","🏆","👽","💀","🐵","🐮","🐩","🐎","💣","👃","👂","🍓","💘","💜","👊","💋","😘","😜","😵","🙏","👋","🚽","💃","💎","🚀","🌙","🎁","⛄","🌊","⛵","🏀","🎱","💰","👶","👸","🐰","🐷","🐍","🐫","🔫","👄","🚲","🍉","💛","💚"]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ScrollView {
+            LazyVGrid(columns: [GridItem(), GridItem(), GridItem()]) {
+                ForEach(emojis[0..<6], id: \.self) { emoji in
+                    CardView(content: emoji).aspectRatio(2/3, contentMode: .fit)
+                }
+            }
         }
         .padding()
     }
 }
 
+
+struct CardView: View {
+    @State var isFaceUp: Bool = true
+    var content: String
+    var body: some View {
+        let shape = RoundedRectangle(cornerRadius: 25)
+        ZStack {
+            if isFaceUp {
+                shape.foregroundColor(.white)
+                shape.strokeBorder(lineWidth: 3)
+                    .foregroundColor(.red)
+                Text(content).font(.largeTitle)
+            } else {
+                shape.fill(.red)
+            }
+        }.onTapGesture {
+            isFaceUp = !isFaceUp
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        ContentView()
+            .preferredColorScheme(.dark)
     }
 }
